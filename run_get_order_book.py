175b4b3a3f46623b283a40fdae4a04d978c38d47
cld-gh <christopherlawrencedavies@gmail.com>
2019-03-14 21:40:06 +0000

    refactor collect

--- run_get_order_book.py
+++ run_get_order_book.py
@@ -1,4 +1,6 @@
-from locrian_collect.schedule_get_order_book import schedule_get_order_book
+#from locrian_collect.schedule_get_order_book import schedule_get_order_book
+from locrian_collect.scheduler import scheduler
 
 
-schedule_get_order_book()
+#schedule_get_order_book()
+scheduler(data_to_record='order_book')
