7f7615883e045779217f0a26adf43163fb09458f
cld-gh <christopherlawrencedavies@gmail.com>
2019-07-01 12:16:19 +0100

    update tests

--- tests/unit/test_scheduler.py
+++ tests/unit/test_scheduler.py
@@ -1,9 +1,16 @@
 """
 Test the scheduling of collecting data.
 """
+import pytest
+
 from locrian_collect.scheduler import delta_time_to_sleep
 
 
+@pytest.mark.parametrize('interval, offset, expected', [
+    [10, 0.1, 5.1],
+    [20, 0.1, 15.1],
+    [100, 0.01, 55.01],
+])
 def test_delta_time_to_sleep(interval, offset, expected, monkeypatch, mocker):
     """Test the time to sleep is as expected."""
     mocker.patch('locrian_collect.scheduler.time.time', return_value=12345)
